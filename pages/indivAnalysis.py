6c1c6674e791088b6d30737d93ce4e17caa515dd
Seongmin0614 <william14@naver.com>
2023-06-07 10:02:15 +0900

    real final

--- pages/indivAnalysis.py
+++ pages/indivAnalysis.py
@@ -78,8 +78,8 @@ layout = html.Div(children=[
                             style = {'width' : '50%'}),
             ], style = {'display':'flex'}),
     dcc.Graph(id='graph3'),
-    html.H1("Total Time of Each Moving Type"),
-    html.Div(children = ["select moving type",
+    html.H1("Total Time of Each Moving Type & App Usage"),
+    html.Div(children = ["select moving type    ",
                 dcc.Dropdown(
                             id = 'types',
                             options = ['STILL', 'WALKING', 'ON_BICYCLE', 'IN_VEHICLE', 'RUNNING'],
@@ -88,7 +88,23 @@ layout = html.Div(children=[
                             value = [],
                             style = {'width' : '50%'}),
             ], style = {'display':'flex'}),
-    dcc.Graph(id='graph2'),
+    dcc.Graph(id='graph2',style={
+           'width' : '700px'}),
+    html.Div(children = ["select apps ",
+                dcc.Dropdown(
+                            id = 'apps2',
+                            multi = True,
+                            clearable = False,
+                            value = [],
+                            style = {'width' : '50%'}),
+            ], style={'position' : 'relative',
+           'left' : '1000px',
+           'bottom' : '500px',
+           'width' : '700px'}),
+    dcc.Graph(id='graph4', style={'position' : 'relative',
+           'left' : '1000px',
+           'bottom' : '500px',
+           'width' : '700px'}),
 
 
 ])
@@ -110,6 +126,16 @@ def if_person(user_id):
 
 @app.callback(
     Output('apps', 'options'),
+    Input('person-id', 'value')
+)
+def if_person(user_id):
+    file_name = 'data/Processed/' + user_id + '_AUE.csv'
+    df = pd.read_csv(file_name)
+    app_list = df.name.unique()
+    return [{'label' : i, 'value' : i} for i in app_list]
+
+@app.callback(
+    Output('apps2', 'options'),
     Input('person-id', 'value')
 )
 def if_person(user_id):
@@ -181,7 +207,6 @@ def update_graph(user_id,starttime, endtime, startdate,enddate, types):
     if os.path.isfile(file_name) == False:
         return fig
     else:
-        #print(startdate + ' ' +starttime)
         st = startdate + ' ' + starttime
         ed = enddate + ' ' + endtime
         try:
@@ -230,5 +255,76 @@ def update_graph(user_id,starttime, endtime, startdate,enddate, types):
         newdf['hours'] = newdf['time'].apply(lambda x:x.total_seconds()/3600)
         newdf = newdf[newdf['moving_type'].isin(types)]
         fig2 = px.bar(newdf, x = 'moving_type', y = 'hours', color = 'moving_type')
-        fig2.update_layout(width = 700)
+        #fig2.update_layout(width = 700)
+        return fig2
+
+
+@app.callback(
+    Output('graph4', 'figure'),
+    Input('person-id', 'value'),
+    Input('start-time', 'value'),
+    Input('end-time', 'value'),
+    Input('start-date', 'date'),
+    Input('end-date', 'date'),
+    Input('apps2', 'value')
+)
+
+def update_graph(user_id,starttime, endtime, startdate,enddate, apps):
+    file_name = 'data/Processed/' + user_id + '_AUE.csv'
+    fig = go.Figure()
+    fig.update_layout(width = 700)
+    if os.path.isfile(file_name) == False:
+        return fig
+    else:
+        st = startdate + ' ' + starttime
+        ed = enddate + ' ' + endtime
+        try:
+            start_range = datetime.datetime.strptime(st, '%Y-%m-%d %H:%M')
+        except:
+            start_range = datetime.datetime.strptime(st, '%Y-%m-%d %H:%M:%S')
+        try:
+            end_range = datetime.datetime.strptime(ed, '%Y-%m-%d %H:%M')
+        except:
+            end_range = datetime.datetime.strptime(ed, '%Y-%m-%d %H:%M:%S')
+        df = pd.read_csv(file_name)
+
+        df = df.drop(['packageName','isSystemApp','isUpdatedSystemApp'], axis = 1)
+        df.columns = ['name', 'start_time', 'end_time']
+        name_time_sum = dict()
+        for name, start, end in zip(df['name'], df['start_time'], df['end_time']):
+            start_time = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S.%f')
+            end_time = datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S.%f')
+            if start_time <= start_range and end_time >= end_range:
+                time_duration = end_range - start_range
+            elif start_time <= start_range <= end_time:
+                time_duration = end_time - start_range
+            elif start_time <= end_range <= end_time:
+                time_duration = end_range - start_time
+            elif start_range <= start_time <= end_time <= end_range:
+                time_duration = end_time - start_time
+            else:
+                time_duration = datetime.timedelta(seconds = 0)
+
+            if name in name_time_sum:
+                name_time_sum[name] += time_duration
+            else:
+                name_time_sum[name] = time_duration
+            '''
+        for name in name_time_sum:
+            temp = name_time_sum[name]
+            tmep2 = datetime.datetime.strptime(temp, '')
+            '''
+        def convert(t):
+            t = int(t)
+            seconds = t % 60
+            temp = t // 60
+            minutes = temp % 60
+            temp = temp // 60
+            hours = temp
+            return datetime.timedelta(hours = hours, minutes = minutes, seconds = seconds)
+        newdf = pd.DataFrame(list(name_time_sum.items()), columns = ['moving_type', 'time'])
+        newdf['hours'] = newdf['time'].apply(lambda x:x.total_seconds()/3600)
+        newdf = newdf[newdf['moving_type'].isin(apps)]
+        fig2 = px.bar(newdf, x = 'moving_type', y = 'hours', color = 'moving_type')
+        #fig2.update_layout(width = 700)
         return fig2
